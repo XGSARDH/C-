dae4683739a76db8ffe13a836f29f6eda3ffa8fb
XGSARDH <xgsardh@163.com>
2024-12-18 00:09:34 +0800

    初始化avl_tree.c源文件

--- experimental_homework/src/avl_tree.c
+++ experimental_homework/src/avl_tree.c
@@ -1,4 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "avl_tree.h"
+#include "status.h"
 
+/* 初始化 AVL 树 */
+Status Avl_Init(AvlTree *root) {
+    return STATUS_TRUE;
+}
+
+/* 默认初始化 AVL 树节点 */
+Status AvlNode_InitDefault(AvlTree *node) {
+    return STATUS_TRUE;
+}
+
+/* 初始化 AVL 树节点 */
+Status AvlNode_Init(AvlTree *node, AvlElementType element) {
+    return STATUS_TRUE;
+}
+
+/* 销毁 AVL 树 */
+Status Avl_Destroy(AvlTree *root) {
+
+    return STATUS_TRUE;
+}
+
+/* 右旋操作 */
+Status Avl_RotateRight(AvlTree *root) {
+
+    return STATUS_TRUE;
+}
+
+/* 左旋操作 */
+Status Avl_RotateLeft(AvlTree *root) {
+
+    return STATUS_TRUE;
+}
+
+/* 插入节点 */
+Status Avl_Insert(AvlTree *root, AvlElementType element) {
+
+    return STATUS_TRUE;
+}
+
+/* 删除节点 */
+Status Avl_Delete(AvlTree *root, AvlElementType element) {
+
+    return STATUS_TRUE;
+}
+
+/* 更新平衡因子 */
+Status Avl_UpdateBalanceFactor(AvlTree root) {
+
+    return STATUS_TRUE;
+}
+
+/* 访问节点值 */
+Status Avl_VisitNode(AvlTree node) {
+
+    return STATUS_TRUE;
+}
+
+/* 中序遍历 */
+Status Avl_InOrderTraverse(AvlTree root) {
+
+    return STATUS_TRUE;
+}
+
+/* 图形化打印 AVL 树 */
+Status Avl_PrintTree(AvlTree root) {
+
+    return STATUS_TRUE;
+}
+
+/* 元素赋值 */
+Status AvlElement_Assign(AvlElementType *destination, AvlElementType *source) {
+
+    return STATUS_TRUE;
+}
+
+/* 比较两个元素是否相等 */
+AvlElementCompareResult AvlElement_IsEqual(AvlElementType a, AvlElementType b) {
+
+    return AVLELEMENT_EQUAL;
+}
